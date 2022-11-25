f0cc633c101db11131991f611ba63bd3b5844f52
n-stott <aeastrolabe@gmail.com>
2022-11-25 01:02:55 +0100

    parse more or

--- include/instructionhandler.h
+++ include/instructionhandler.h
@@ -95,14 +95,28 @@ namespace x86 {
         virtual void exec(And<Addr<Size::DWORD, BIS>, R32>) = 0;
         virtual void exec(And<Addr<Size::DWORD, BISD>, R32>) = 0;
 
-        virtual void exec(Or<R32, Imm<u32>>) = 0;
+        virtual void exec(Or<R8, R8>) = 0;
+        virtual void exec(Or<R8, Imm<u8>>) = 0;
+        virtual void exec(Or<R8, Addr<Size::BYTE, B>>) = 0;
+        virtual void exec(Or<R8, Addr<Size::BYTE, BD>>) = 0;
+        virtual void exec(Or<R16, Addr<Size::WORD, B>>) = 0;
+        virtual void exec(Or<R16, Addr<Size::WORD, BD>>) = 0;
         virtual void exec(Or<R32, R32>) = 0;
+        virtual void exec(Or<R32, Imm<u32>>) = 0;
         virtual void exec(Or<R32, Addr<Size::DWORD, B>>) = 0;
         virtual void exec(Or<R32, Addr<Size::DWORD, BD>>) = 0;
         virtual void exec(Or<R32, Addr<Size::DWORD, BIS>>) = 0;
         virtual void exec(Or<R32, Addr<Size::DWORD, BISD>>) = 0;
+        virtual void exec(Or<Addr<Size::BYTE, B>, R8>) = 0;
+        virtual void exec(Or<Addr<Size::BYTE, B>, Imm<u8>>) = 0;
+        virtual void exec(Or<Addr<Size::BYTE, BD>, R8>) = 0;
+        virtual void exec(Or<Addr<Size::BYTE, BD>, Imm<u8>>) = 0;
+        virtual void exec(Or<Addr<Size::WORD, B>, R16>) = 0;
+        virtual void exec(Or<Addr<Size::WORD, BD>, R16>) = 0;
         virtual void exec(Or<Addr<Size::DWORD, B>, R32>) = 0;
+        virtual void exec(Or<Addr<Size::DWORD, B>, Imm<u32>>) = 0;
         virtual void exec(Or<Addr<Size::DWORD, BD>, R32>) = 0;
+        virtual void exec(Or<Addr<Size::DWORD, BD>, Imm<u32>>) = 0;
         virtual void exec(Or<Addr<Size::DWORD, BIS>, R32>) = 0;
         virtual void exec(Or<Addr<Size::DWORD, BISD>, R32>) = 0;
 
