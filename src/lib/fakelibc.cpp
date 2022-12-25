de75b4b29b472569b0f37c9db0145e9520baef1c
Nikolas <nikolas.stott@hotmail.fr>
2022-12-25 11:01:39 +0100

    LibC: memcpy and free

--- src/lib/fakelibc.cpp
+++ src/lib/fakelibc.cpp
@@ -46,4 +46,18 @@ extern "C" {
         return ptr;
     }
 
+    void fakelibc$free(void* ptr) {
+        (void)ptr;
+    }
+
+    void* fakelibc$memcpy(void* dest, const void* src, size_t n) {
+        unsigned char* d = (unsigned char*)dest;
+        const unsigned char* s = (const unsigned char*)src;
+        while(n > 0) {
+            *d++ = *s++;
+            --n;
+        }
+        return dest;
+    }
+
 }
