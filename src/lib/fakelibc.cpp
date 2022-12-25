b6b76dc4729545e10c88624c92a4986eb77dd1db
Nikolas <nikolas.stott@hotmail.fr>
2022-12-25 12:36:56 +0100

    LibC: memmove

--- src/lib/fakelibc.cpp
+++ src/lib/fakelibc.cpp
@@ -60,4 +60,14 @@ extern "C" {
         return dest;
     }
 
+    void* fakelibc$memmove(void* dest, const void* src, size_t n) {
+        unsigned char* d = (unsigned char*)dest;
+        const unsigned char* s = (const unsigned char*)src;
+        while(n > 0) {
+            *d++ = *s++;
+            --n;
+        }
+        return dest;
+    }
+
 }
