3623ab17d4bf9fea220e1e4228a072115b7da276
Nikolas <nikolas.stott@hotmail.fr>
2023-01-09 01:16:05 +0100

    Tests: improve stringstream test

--- tests/src/stringstream.cpp
+++ tests/src/stringstream.cpp
@@ -1,18 +1,25 @@
 #include <sstream>
 #include <locale.h>
+#include <cstdio>
 
 void testA() {
     std::stringstream ss;
+    std::string s = ss.str();
+    std::puts(s.c_str());
 }
 
 void testB() {
     std::stringstream ss;
     ss << 123;
+    std::string s = ss.str();
+    std::puts(s.c_str());
 }
 
 void testC() {
     std::stringstream ss;
     ss << "abc";
+    std::string s = ss.str();
+    std::puts(s.c_str());
 }
 
 int main() {
