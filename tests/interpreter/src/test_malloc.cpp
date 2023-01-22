e9b9247a9a189bcc4c072d41d1090d70fe80426c
Nikolas <nikolas.stott@hotmail.fr>
2023-01-22 13:11:23 +0100

    Tests: add heap-filling test

--- tests/interpreter/src/test_malloc.cpp
+++ tests/interpreter/src/test_malloc.cpp
@@ -11,6 +11,39 @@ void test1() {
     free(ptr);
 }
 
+struct VoidPtrList {
+    void* elem = nullptr;
+    VoidPtrList* next = nullptr;
+};
+
+void test2() {
+    VoidPtrList allocations;
+    VoidPtrList* current = &allocations;
+    for(int i = 0; i < 0x100; ++i) {
+        current->elem = malloc(0x100000);
+        if(!current->elem) {
+            puts("Failed to allocate 0x10000 bytes");
+            break;
+        }
+        current->next = (VoidPtrList*)malloc(sizeof(VoidPtrList));
+        if(!current->next) {
+            puts("Failed to allocate VoidPtrList");
+            break;
+        }
+        current->next->elem = nullptr;
+        current->next->next = nullptr;
+        current = current->next;
+    }
+    current = &allocations;
+    while(!!current) {
+        VoidPtrList* next = current->next;
+        free(current->elem);
+        if(current != &allocations) free(current);
+        current = next;
+    }
+}
+
 int main() {
     test1();
+    test2();
 }
