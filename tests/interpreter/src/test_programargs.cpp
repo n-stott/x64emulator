a71186365515e75c1e73ed1533b1d41c5af20f5b
Nikolas <nikolas.stott@hotmail.fr>
2023-01-22 12:46:17 +0100

    Tests: add program argument test

---
+++ tests/interpreter/src/test_programargs.cpp
@@ -0,0 +1,11 @@
+#include <fmt/core.h>
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+    fmt::print("argc={}\n", argc);
+    for(int i = 0; i < argc; ++i) {
+        puts(argv[i]);
+        // fmt::print("argv[{}]={}\n", i, argv[i]);
+    }
+    return 0;
+}
