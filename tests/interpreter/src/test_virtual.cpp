60e215f1fcbbdc367016fbda45e954d5a46b74fe
Nikolas <nikolas.stott@hotmail.fr>
2023-03-10 07:31:50 +0100

    Tests: virtual functions

---
+++ tests/interpreter/src/test_virtual.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+
+void testA() {
+    struct Base {
+        virtual ~Base() { std::puts("~Base();"); }
+    };
+    struct Derived : public Base {
+        ~Derived() { std::puts("~Derived();"); }
+    };
+
+    Derived d;
+    (void)d;
+}
+
+void testB() {
+    struct Base {
+        virtual void f() { std::puts("Base::f()"); }
+    };
+    struct Derived : public Base {
+        void f() override { std::puts("Derived::f();"); }
+    };
+
+    Derived d;
+    d.f();
+    d.Base::f();
+}
+
+void testC() {
+    struct Base {
+        virtual void f() { std::puts("Base::f()"); }
+    };
+    struct Derived1 : public Base {
+        void f() override { std::puts("Derived1::f();"); }
+    };
+    struct Derived2 : public Base {
+        void f() override { std::puts("Derived2::f();"); }
+    };
+
+    Derived1 d1;
+    Derived2 d2;
+    Base* b = &d1;
+    b->f();
+    b = &d2;
+    b->f();
+}
+
+int main() {
+    testA();
+    testB();
+    testC();
+}
