5e6100199695f266b6426e4fb973cd13c59a76f4
Nikolas <nikolas.stott@hotmail.fr>
2023-03-28 23:19:43 +0200

    Fix compilation

---
+++ include/utils/utils.h
@@ -0,0 +1,24 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <cstdlib>
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using i8 = char;
+using i16 = short;
+using i32 = int;
+using i64 = long long;
+
+static_assert(sizeof(u8) == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
+static_assert(sizeof(i8) == 1);
+static_assert(sizeof(i16) == 2);
+static_assert(sizeof(i32) == 4);
+static_assert(sizeof(i64) == 8);
+
+#endif
