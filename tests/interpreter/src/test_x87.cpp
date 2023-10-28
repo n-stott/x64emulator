54c55fd6359cc06d7f4e23792149ce1dd7f85fd4
Nikolas <nikolas.stott@hotmail.fr>
2023-10-28 13:45:59 +0200

    Add x87 fpu test and mark some tests as failing for now

---
+++ tests/interpreter/src/test_x87.cpp
@@ -0,0 +1,14 @@
+long double add(long double x, long double y) {
+    return x + y;
+}
+
+int main() {
+    long double a = 0;
+    long double b = 0.0;
+    long double c = 1.0;
+    long double d = 1;
+    long double x = 1.5;
+    long double y = -200;
+    long double z = add(x, y);
+    return 0;
+}
