6509feeb6d8123717ea64a3a1da83195ed477180
Nikolas <nikolas.stott@hotmail.fr>
2023-11-04 15:48:33 +0100

    Tests: also test opening file in binary mode

--- tests/interpreter/src/test_readfile.cpp
+++ tests/interpreter/src/test_readfile.cpp
@@ -1,10 +1,24 @@
 #include <fstream>
 #include <iostream>
 
-int main() {
+int readText() {
     std::ifstream file("testfile.txt");
     if(!file.good()) return 1;
     std::string s;
     file >> s;
     std::cout << s << std::endl;
+    return 0;
+}
+
+int readBin() {
+    std::ifstream file("testfile.txt", std::ios::in | std::ios::binary);
+    if(!file.good()) return 2;
+    return 0;
+}
+
+int main() {
+    int rc = 0;
+    rc |= readText();
+    rc |= readBin();
+    return rc;
 }
