446c2012ca9097961fe5cba35e84ce5c1766b7b8
Nikolas <nikolas.stott@hotmail.fr>
2023-11-24 09:20:16 +0100

    TCB: add pointer guard

--- include/interpreter/tcb.h
+++ include/interpreter/tcb.h
@@ -15,13 +15,16 @@ namespace x64 {
             tcb.padding_[2] = 0;
             tcb.padding_[3] = 0;
             tcb.stackCanary_ = 0x1234567887654321;
+            tcb.pointerGuard_ = 0;
             return tcb;
         }
 
     private:
+        // copying the layout of https://github.com/lattera/glibc/blob/895ef79e04a953cac1493863bcae29ad85657ee1/sysdeps/x86_64/nptl/tls.h
         u64 fsBase_; // fs:0x0
         u64 padding_[4]; // fs:0x8
         u64 stackCanary_; // fs:0x28
+        u64 pointerGuard_; // fs:0x30
     };
 
 }
