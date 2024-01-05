6bbd06d65dfe2cf69d9204a470d7a27f9e9e897f
Nikolas <nikolas.stott@hotmail.fr>
2024-01-05 08:41:53 +0100

    Tests: add unaligned sse_strcmp core test

---
+++ tests/cpu/src/test_sse_strcmp.cpp
@@ -0,0 +1,39 @@
+#include "types.h"
+#include "interpreter/cpuimpl.h"
+#include "fmt/core.h"
+
+u32 strlen_core_native(u128 a, u128 b) {
+    u128 z { 0, 0 };
+    u32 res { 0 };
+    asm volatile("pxor %0, %0" : "+x"(z));
+    asm volatile("pcmpeqb %1, %0" : "+x"(z) : "x"(a));
+    asm volatile("pslldq %1, %0" : "+x"(b) : "i"(0xb));
+    asm volatile("pcmpeqb %1, %0" : "+x"(b) : "x"(a));
+    asm volatile("psubb %1, %0" : "+x"(z) : "x"(b));
+    asm volatile("pmovmskb %1, %0" : "+r"(res) : "x"(z));
+    return res;
+}
+
+u32 strlen_core_virtual(u128 xmm1, u128 xmm2) {
+    using namespace x64;
+    u128 xmm0 { 0, 0 };
+    xmm0 = Impl::pcmpeqb(xmm0, xmm1);
+    xmm2 = Impl::pslldq(xmm2, 0xb);
+    xmm2 = Impl::pcmpeqb(xmm2, xmm1);
+    xmm2 = Impl::psubb(xmm2, xmm0);
+    u32 res = Impl::pmovmskb(xmm2);
+    return res;
+}
+
+int main() {
+    u128 a { 0x2e6362696c003832, 0x3166627573783436 };
+    u128 b { 0x362e6f732e2b, 0x2b6364747362696c };
+    u32 res_native = strlen_core_native(a, b);
+    u32 res_virtual = strlen_core_virtual(a, b);
+    if(res_native != res_virtual) {
+        fmt::print("res_native={}\n", res_native);
+        fmt::print("res_virtual={}\n", res_virtual);
+        return 1;
+    }
+    return 0;
+}
