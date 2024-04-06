0051f43d9eb76d3619189a59c3f5c5ca5be49c72
Nikolas <nikolas.stott@hotmail.fr>
2024-04-06 10:00:40 +0200

    Tests: add second threadlocal test.

---
+++ tests/interpreter/src/test_threadlocal2.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <thread>
+
+struct S {
+    static thread_local const char* message;
+};
+
+thread_local const char* S::message = "this is a thread_local character literal";
+
+int test1() {
+    return puts(S::message);
+}
+
+int main() {
+    try {
+        std::thread t1([]() {
+            S::message = "threadlocal message from thread 1";
+            std::puts(S::message);
+        });
+        std::thread t2([]() {
+            S::message = "threadlocal message from thread 2";
+            std::puts(S::message);
+        });
+        S::message = "threadlocal message from main thread";
+        std::puts(S::message);
+        t1.join();
+        t2.join();
+    } catch(std::exception& e) {
+        std::puts(e.what());
+    }
+}
