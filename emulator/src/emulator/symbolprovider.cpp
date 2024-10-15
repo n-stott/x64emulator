9aa550e61eb277dbf01f737ddce123489ff4e531
Nikolas <nikolas.stott@hotmail.fr>
2024-10-15 07:36:39 +0200

    SymbolProvider: fail softly when unable to parse symbol location.
    Symbol loading fails when the file is a .dsym.

--- emulator/src/emulator/symbolprovider.cpp
+++ emulator/src/emulator/symbolprovider.cpp
@@ -55,6 +55,25 @@ namespace emulator {
         return s;
     }
 
+    std::unique_ptr<elf::Elf> tryReadDebugElf(std::optional<elf::Section> debugLinkSection, std::optional<elf::Section> buildIdSection) {
+        if(!debugLinkSection) return {};
+        if(!buildIdSection) return {};
+        std::string debugLink(reinterpret_cast<const char*>(debugLinkSection->begin));
+        if(buildIdSection->size() < 3*sizeof(u64)) return {};
+        u32 namesize = *(u32*)buildIdSection->begin;
+        if(namesize != 4) return {};
+        u32 descsize = *(u32*)(buildIdSection->begin + 1*sizeof(u32));
+        if(descsize != 20) return {};
+        std::string name(reinterpret_cast<const char*>(buildIdSection->begin + 3*sizeof(u32)));
+        const u8* desc = buildIdSection->begin + 3*sizeof(u32) + namesize;
+        if(name != "GNU") return {};
+        // u32 ntype = *(u32*)(buildIdSection->begin + 2*sizeof(u32));
+        // verify(ntype == NT_GNU_BUILD_ID);
+        std::vector<u8> buildId(desc, desc+descsize);
+        std::string debugElfFilename = fmt::format("/usr/lib/debug/.build-id/{:x}/{}", buildId[0], debugLink);
+        return elf::ElfReader::tryCreate(debugElfFilename);
+    }
+
     void SymbolProvider::tryRetrieveSymbolsFromExecutable(const std::string& filename, u64 loadAddress) {
         if(filename.empty()) return;
         if(std::find(symbolicatedElfs_.begin(), symbolicatedElfs_.end(), filename) != symbolicatedElfs_.end()) return;
@@ -70,26 +89,10 @@ namespace emulator {
 
         std::optional<elf::Section> debugLinkSection = elf64->sectionFromName(".gnu_debuglink");
         std::optional<elf::Section> buildIdSection = elf64->sectionFromName(".note.gnu.build-id");
-        if(!!debugLinkSection && !!buildIdSection) {
-            std::string debugLink(reinterpret_cast<const char*>(debugLinkSection->begin));
-            verify(buildIdSection->size() >= 3*sizeof(u64));
-            u32 namesize = *(u32*)buildIdSection->begin;
-            verify(namesize == 4);
-            u32 descsize = *(u32*)(buildIdSection->begin + 1*sizeof(u32));
-            verify(descsize == 20);
-            std::string name(reinterpret_cast<const char*>(buildIdSection->begin + 3*sizeof(u32)));
-            const u8* desc = buildIdSection->begin + 3*sizeof(u32) + namesize;
-            verify(name == "GNU");
-            // u32 ntype = *(u32*)(buildIdSection->begin + 2*sizeof(u32));
-            // verify(ntype == NT_GNU_BUILD_ID);
-            std::vector<u8> buildId(desc, desc+descsize);
-
-            std::string debugElfFilename = fmt::format("/usr/lib/debug/.build-id/{:x}/{}", buildId[0], debugLink);
-            std::unique_ptr<elf::Elf> debugElf = elf::ElfReader::tryCreate(debugElfFilename);
-            if(!!debugElf) {
-                // replace the elf file to recover the symbols
-                elf64.reset(static_cast<elf::Elf64*>(debugElf.release()));
-            }
+        std::unique_ptr<elf::Elf> debugElf = tryReadDebugElf(debugLinkSection, buildIdSection);
+        if(!!debugElf) {
+            // replace the elf file to recover the symbols
+            elf64.reset(static_cast<elf::Elf64*>(debugElf.release()));
         }
 
         size_t nbExecutableProgramHeaders = 0;
