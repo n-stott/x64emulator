e1adda9464fd1781526e853bd19044f6b237c551
Nikolas <nikolas.stott@hotmail.fr>
2025-02-12 22:21:47 +0100

    Emulator: use the argparse library to parse the command line and parse emulator parameters.
    The parameters are: log the syscalls, profile the program and disable the jit.

--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -1,5 +1,6 @@
 #include "emulator/emulator.h"
 #include "signalhandler.h"
+#include <argparse/argparse.hpp>
 #include <fmt/core.h>
 #include <string>
 #include <vector>
@@ -14,16 +15,51 @@ void crashHandler(int signum) {
 }
 
 int main(int argc, char* argv[], char* envp[]) {
-    if(argc < 2) {
-        fmt::print(stderr, "Missing argument: path to program\n");
+
+    argparse::ArgumentParser parser("emulator");
+
+    parser.add_argument("--syscalls")
+           .help("display syscalls")
+           .default_value(false)
+           .implicit_value(true);
+
+    parser.add_argument("--profile")
+           .help("profile the main thread")
+           .default_value(false)
+           .implicit_value(true);
+
+    parser.add_argument("--nojit")
+           .help("disable the JIT")
+           .default_value(false)
+           .implicit_value(true);
+
+    parser.add_argument("command")
+           .remaining();
+
+    try {
+        parser.parse_args(argc, argv);
+    } catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
         return 1;
     }
 
-    std::string programPath = argv[1];
-
+    std::string programPath;
     std::vector<std::string> arguments;
-    for(int arg = 2; arg < argc; ++arg) {
-        arguments.push_back(argv[arg]);
+
+    try {
+        auto args = parser.get<std::vector<std::string>>("command");
+        if(args.empty()) {
+            fmt::print("No program path provided\n");
+            return 1;
+        }
+        programPath = args[0];
+        for(size_t i = 1; i < args.size(); ++i) {
+            arguments.push_back(args[i]);
+        }
+    } catch (std::logic_error& e) {
+        std::cout << "No args provided" << std::endl;
+        return 1;
     }
 
     std::vector<std::string> environmentVariables;
@@ -35,9 +71,9 @@ int main(int argc, char* argv[], char* envp[]) {
     SignalHandler<SIGINT> sigintHandler(&crashHandler);
 
     emulator::Emulator emulator;
-    emulator.setLogSyscalls(false);
-    emulator.setProfiling(false);
-    emulator.setEnableJit(true);
+    emulator.setLogSyscalls(parser["--syscalls"] == true);
+    emulator.setProfiling(parser["--profile"] == true);
+    emulator.setEnableJit(parser["--nojit"] == false);
     bool ok = emulator.run(programPath, arguments, environmentVariables);
 
     return !ok;
