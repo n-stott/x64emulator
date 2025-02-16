7853683fa2ed4c705bf51de0056a39c1d250bb4b
Nikolas <nikolas.stott@hotmail.fr>
2025-02-16 11:07:43 +0100

    Tests: add mul and pop compiler tests

---
+++ tests/compiler/src/test_mul.cpp
@@ -0,0 +1,88 @@
+#include "x64/compiler/assembler.h"
+#include "x64/disassembler/capstonewrapper.h"
+#include "verify.h"
+
+using namespace x64;
+
+void testMul32(R32 src) {
+    Assembler assembler;
+    assembler.mul(src);
+    std::vector<u8> code = assembler.code();
+    auto disassembly = CapstoneWrapper::disassembleRange(code.data(), code.size(), 0x0);
+    verify(disassembly.instructions.size() == 1);
+    const auto& ins = disassembly.instructions[0];
+    verify(ins.insn() == Insn::MUL_RM32);
+    RM32 dissrc = ins.op0<RM32>();
+    verify(dissrc.isReg);
+    verify(dissrc.reg == src);
+}
+
+void testMul32() {
+    std::array<R32, 16> regs {{
+        R32::EAX,
+        R32::ECX,
+        R32::EDX,
+        R32::EBX,
+        R32::ESP,
+        R32::EBP,
+        R32::ESI,
+        R32::EDI,
+        R32::R8D,
+        R32::R9D,
+        R32::R10D,
+        R32::R11D,
+        R32::R12D,
+        R32::R13D,
+        R32::R14D,
+        R32::R15D,
+    }};
+    for(auto src : regs) {
+        testMul32(src);
+    }
+}
+
+void testMul64(R64 src) {
+    Assembler assembler;
+    assembler.mul(src);
+    std::vector<u8> code = assembler.code();
+    auto disassembly = CapstoneWrapper::disassembleRange(code.data(), code.size(), 0x0);
+    verify(disassembly.instructions.size() == 1);
+    const auto& ins = disassembly.instructions[0];
+    verify(ins.insn() == Insn::MUL_RM64);
+    RM64 dissrc = ins.op0<RM64>();
+    verify(dissrc.isReg);
+    verify(dissrc.reg == src);
+}
+
+void testMul64() {
+    std::array<R64, 16> regs {{
+        R64::RAX,
+        R64::RCX,
+        R64::RDX,
+        R64::RBX,
+        R64::RSP,
+        R64::RBP,
+        R64::RSI,
+        R64::RDI,
+        R64::R8,
+        R64::R9,
+        R64::R10,
+        R64::R11,
+        R64::R12,
+        R64::R13,
+        R64::R14,
+        R64::R15,
+    }};
+    for(auto src : regs) {
+        testMul64(src);
+    }
+}
+
+int main() {
+    try {
+        testMul32();
+        testMul64();
+    } catch(...) {
+        return 1;
+    }
+}
---
+++ tests/compiler/src/test_scenario_04.cpp
@@ -0,0 +1,84 @@
+#include "x64/cpu.h"
+#include "x64/mmu.h"
+#include "x64/compiler/compiler.h"
+#include <sys/mman.h>
+
+int main(int argc, char**) {
+    using namespace x64;
+    Mmu mmu;
+    Cpu cpu(mmu);
+
+    std::array<X64Instruction, 11> instructions {{
+        X64Instruction::make(0x0, Insn::NOP, 1),
+        X64Instruction::make(0x1, Insn::POP_R64, 1, R64::RBX),
+        X64Instruction::make(0x2, Insn::POP_R64, 1, R64::R14),
+        X64Instruction::make(0x3, Insn::POP_R64, 1, R64::R13),
+        X64Instruction::make(0x4, Insn::POP_R64, 1, R64::R12),
+        X64Instruction::make(0x5, Insn::POP_R64, 1, R64::R11),
+        X64Instruction::make(0x6, Insn::POP_R64, 1, R64::R10),
+        X64Instruction::make(0x7, Insn::MOV_R64_R64, 1, R64::RSP, R64::RBP),
+        X64Instruction::make(0x8, Insn::POP_R64, 1, R64::RSP),
+        X64Instruction::make(0x9, Insn::POP_R64, 1, R64::RBP),
+        X64Instruction::make(0xa, Insn::RET, 1),
+    }};
+
+    auto bb = cpu.createBasicBlock(instructions.data(), instructions.size());
+
+    u64 stack1Top = mmu.mmap(0, 0x1000, BitFlags<x64::PROT>{x64::PROT::READ, x64::PROT::WRITE}, BitFlags<x64::MAP>{x64::MAP::PRIVATE, x64::MAP::ANONYMOUS})
+                    + 0x1000;
+    u64 stack2Top = mmu.mmap(0, 0x1000, BitFlags<x64::PROT>{x64::PROT::READ, x64::PROT::WRITE}, BitFlags<x64::MAP>{x64::MAP::PRIVATE, x64::MAP::ANONYMOUS})
+                    + 0x1000;
+
+    cpu.set(R64::RBX, 0xb);
+    cpu.set(R64::R10, 0x10);
+    cpu.set(R64::R11, 0x11);
+    cpu.set(R64::R12, 0x12);
+    cpu.set(R64::R13, 0x13);
+    cpu.set(R64::R14, 0x14);
+    cpu.set(R64::RIP, 0xabcd);
+
+    cpu.set(R64::RBP, stack1Top);
+    cpu.set(R64::RSP, stack2Top);
+
+    cpu.push64(cpu.get(R64::RIP));
+    cpu.push64(cpu.get(R64::RBP));
+    cpu.push64(cpu.get(R64::RSP));
+    cpu.set(R64::RBP, cpu.get(R64::RSP));
+    cpu.push64(cpu.get(R64::R10));
+    cpu.push64(cpu.get(R64::R11));
+    cpu.push64(cpu.get(R64::R12));
+    cpu.push64(cpu.get(R64::R13));
+    cpu.push64(cpu.get(R64::R14));
+    cpu.push64(cpu.get(R64::RBX));
+
+    u64 ticks { 0 };
+
+    if(argc != 1) {
+        cpu.exec(bb);
+    } else {
+        auto nativebb = Compiler::tryCompile(bb);
+        if(!nativebb) {
+            fmt::print(stderr, "failed to compile\n");
+            return 1;
+        }
+
+        void* ptr = ::mmap(nullptr, 0x1000, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
+        if(ptr == (void*)MAP_FAILED) return 1;
+        ::memcpy(ptr, nativebb->nativecode.data(), nativebb->nativecode.size());
+        cpu.exec((NativeExecPtr)ptr, &ticks);
+    }
+
+    fmt::print("R10={:#x}\n", cpu.get(R64::R10));
+    fmt::print("R11={:#x}\n", cpu.get(R64::R11));
+    fmt::print("R12={:#x}\n", cpu.get(R64::R12));
+    fmt::print("R13={:#x}\n", cpu.get(R64::R13));
+    fmt::print("R14={:#x}\n", cpu.get(R64::R14));
+    fmt::print("RBX={:#x}\n", cpu.get(R64::RBX));
+    fmt::print("RSP={:#x}\n", cpu.get(R64::RSP));
+    fmt::print("RBP={:#x}\n", cpu.get(R64::RBP));
+    fmt::print("RIP={:#x}\n", cpu.get(R64::RIP));
+
+    // if(cpu.get(R64::RAX) != 0x58) return 1;
+
+    return 0;
+}
