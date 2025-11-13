2467973217ba5af4d050eb2026c834611cffc1e0
Nikolas <nikolas.stott@hotmail.fr>
2025-11-13 21:20:31 +0100

    Shm: fail with ENOMEM instead of throwing if attaching the shared memory segment fails

--- emulator/src/kernel/linux/shm/sharedmemorysegment.cpp
+++ emulator/src/kernel/linux/shm/sharedmemorysegment.cpp
@@ -42,7 +42,7 @@ namespace kernel::gnulinux {
         // THIS IS A MASSIVE HACK :-p
         // We reserve the range in the Mmu and in host memory and remap the shared region on top.
         auto addr = mmu_.mmap(0, size_, prot, flags);
-        verify(!!addr, "Unable to mmap region for shared memory");
+        if(!addr) return ErrnoOr<u64>(-ENOMEM);
         void* ret = ::shmat(id_, (void*)(mmu_.base() + addr.value()), SHM_REMAP);
         if(ret == (void*)(-1)) {
             return ErrnoOr<u64>(-errno);
